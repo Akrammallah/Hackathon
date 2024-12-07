1ed51b8f99b421c66b9317d16fe78cef3280b7ff
Akrammallah <153257896+Akrammallah@users.noreply.github.com>
2024-12-07 20:56:43 +0500

    hackathon project

---
+++ src/app/componants/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className='bg-red-600'>welcome</h1>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+    <h1 className='bg-yellow-600'>hello</h1>  
+    </>
+  )
+}
+
+export default page
